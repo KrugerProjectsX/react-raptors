5b6a0212bd4804694c006e5a52dd799a97e9ac8b
HugoLeg86 <hlegarda@krugerschool.edu.ec>
2024-03-11 19:22:36 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
-
+import Login from "./Pages/Login";
+import AddFlat from "./Pages/AddFlat";
 function App() {
   return (
    <Routes>
-      <Route path={"/"} element={<Home/>}/>
+      <Route path={"/"} element={<Login/>}/>
+      <Route path={"/dashboard"} element={<Home/>}/>
+      <Route path={"/flat/new"} element={<AddFlat/>}/>
    </Routes>
   );
 };
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,48 @@
+import {Box, Button, TextField} from  "@mui/material"
+import { where, query,collection,getDocs,  } from "firebase/firestore";
+import { useRef } from "react";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+    const email = useRef("");
+    const password = useRef("")
+    const usersRef = collection(db, 'users ');
+    const navigate = useNavigate();
+    const login = async (e) => {
+        e.preventDefault();
+        const search = query(usersRef, where('email', '==', email.current.value))
+        const result = await getDocs(search);
+        if (result.docs.lenght > 0 ){
+            const user = result.docs[0].data()
+            const user_id = result.docs[0].id
+            if (user.password === password.current.value){
+                console.log("login success")
+                console.log("redirect")
+                
+                localStorage.setItem('user_loged', JSON.stringify(user_id));
+                navigate('/dashboard', {replace: true});
+                
+            }else{
+                console.log("login failure")
+            }
+        } else {
+            alert("No User Found! Please Sign")
+            }
+            }
+        
+    ;
 
     return(
         <>
+            <h1>Login</h1>
+       
+        <Box component="form" onSubmit={login} sx= {{p:2, border:'1px dashed grey'}}>
+                <TextField label="Email" inputRef={email} className></TextField>
+                <TextField label="Password" inputRef={password}></TextField>
+                <Button type="submit" variant="contained">Login</Button>
+                
+        </Box>
         </>
     )
 };
